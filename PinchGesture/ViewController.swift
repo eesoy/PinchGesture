ffe523e4b4999a01a0a0b505784fd66f36d58d68
SO YOUNG <LEESOYOUNG@itui-macbook-pro.local>
2018-01-06 13:07:07 +0900

    Text

--- PinchGesture/ViewController.swift
+++ PinchGesture/ViewController.swift
@@ -10,9 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblPinch: UILabel!
+    
+    var initFontSize: CGFloat = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(gestureHandler(gesture:))) //시뮬레이터 사용방법 alt + cursor
+        view.addGestureRecognizer(pinch)
+    }
+    
+    @objc func gestureHandler(gesture: UIPinchGestureRecognizer) {
+        print("UIPinchGestureRecognizer")
+        if gesture.state == UIGestureRecognizerState.began {
+            initFontSize = lblPinch.font.pointSize
+        } else {
+            lblPinch.font = lblPinch.font.withSize(initFontSize * gesture.scale)
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
